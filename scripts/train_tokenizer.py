41f1a3872099da3d0741d41e666059d524dea93b
blender <blenderwang.cs10@nycu.edu.tw>
2024-07-10 20:03:36 +0800

    add postprocessing to tokenizer

--- scripts/train_tokenizer.py
+++ scripts/train_tokenizer.py
@@ -2,9 +2,10 @@ from functools import partial
 from pathlib import Path
 
 import typer
-from tokenizers import Tokenizer
+from tokenizers import Encoding, Tokenizer
 from tokenizers.models import BPE
 from tokenizers.pre_tokenizers import Split
+from tokenizers.processors import RobertaProcessing
 from tokenizers.trainers import BpeTrainer
 
 
@@ -15,7 +16,11 @@ def file_iterator(files: list[Path]):
                 yield line.split()[0]
 
 
-def main(
+app = typer.Typer(pretty_exceptions_enable=False)
+
+
+@app.command()
+def train(
     data_path: Path,
     regex_model: Path,
     save_path: Path,
@@ -36,5 +41,36 @@ def main(
     tokenizer.save(str(save_path))
 
 
+@app.command()
+def add_postprocessor(
+    tokenizer_file: Path,
+    save_path: Path,
+):
+
+    test_smi = "c1cc2c(nc1)n(c(=O)o2)CC[NH+]3CCCCC3"
+    print("test smiles:", test_smi)
+
+    def test_tok(tokenizer: Tokenizer, test_smi: str):
+        tok_output: Encoding = tokenizer.encode(test_smi)
+        print("encoded output:", tok_output.ids)
+
+        decoded_output = tokenizer.decode(tok_output.ids, skip_special_tokens=False)
+        print("decoded output:", decoded_output)
+
+    tokenizer: Tokenizer = Tokenizer.from_file(str(tokenizer_file))
+    print("before adding postprocessor:")
+    test_tok(tokenizer, test_smi)
+
+    sep_str_and_id = ("<sep>", tokenizer.token_to_id("<sep>"))
+    cls_str_and_id = ("<cls>", tokenizer.token_to_id("<cls>"))
+
+    tokenizer.post_processor = RobertaProcessing(
+        sep=sep_str_and_id, cls=cls_str_and_id, add_prefix_space=False
+    )
+    print("after adding postprocessor:")
+    test_tok(tokenizer, test_smi)
+    tokenizer.save(str(save_path))
+
+
 if __name__ == "__main__":
-    typer.run(main)
+    app()
