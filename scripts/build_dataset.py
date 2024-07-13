fdf7e6f81bf26af7f285f6a48ad5c53cf70452d5
blender <blenderwang.cs10@nycu.edu.tw>
2024-07-13 22:18:45 +0800

    simplify dataset building script

--- scripts/build_dataset.py
+++ scripts/build_dataset.py
@@ -1,3 +1,4 @@
+from functools import partial
 from pathlib import Path
 
 import datasets
@@ -7,11 +8,54 @@ from rdkit import Chem
 from transformers import PreTrainedTokenizerFast
 
 
-def file_iterator(files: list[Path]):
+def normalize_smiles(
+    smiles: str, canonical: bool = True, isomeric: bool = False
+) -> str | None:
+    try:
+        res = Chem.MolToSmiles(
+            Chem.MolFromSmiles(smiles),
+            canonical=canonical,
+            isomericSmiles=isomeric,
+        )
+    except:
+        return None
+    return res
+
+
+def filter_func(
+    smiles: str,
+    tokenizer: PreTrainedTokenizerFast,
+    unk_id: int,
+    max_len: int = 512,  # after tokenization
+    min_len: int = 8,  # after tokenization
+    canonical: bool = True,
+    isomeric: bool = False,
+    filter_unk: bool = True,
+):
+    smiles = normalize_smiles(smiles, canonical, isomeric)
+    if smiles is None:
+        return None
+    tok_output = tokenizer.encode_plus(
+        smiles,
+        return_token_type_ids=False,
+        return_attention_mask=False,
+        return_length=True,
+    )
+    l = tok_output["length"][0]
+    if l > max_len or l < min_len:
+        return None
+    if filter_unk and (unk_id in tok_output["input_ids"]):
+        return None
+    return smiles
+
+
+def file_iterator(files: list[Path], smiles_filter):
     for file in files:
         with open(file, "r") as f:
             for line in f:
-                smiles = line.split()[0]
+                smiles: str | None = smiles_filter(line.split(maxsplit=1)[0])
+                if smiles is None:
+                    continue
                 yield {"smiles": smiles, "strlen": len(smiles)}
 
 
@@ -22,122 +66,50 @@ app = typer.Typer(pretty_exceptions_enable=False)
 def build(
     data_path: Path,
     save_path: Path,
-    num_proc: int = 4,
-    num_shards: int = 10,
-):
-    files = list(data_path.glob("*.txt")) if data_path.is_dir() else [data_path]
-
-    dataset = datasets.Dataset.from_generator(
-        file_iterator,
-        gen_kwargs={
-            "files": files,
-        },
-        num_proc=num_proc,
-    )
-
-    dataset.set_format("np")
-    dataset.save_to_disk(save_path, num_shards=num_shards, num_proc=num_proc)
-
-
-@app.command()
-def filter(
-    dataset_path: Path,
-    save_path: Path,
     pretrained_tokenizer: Path,
     max_len: int = 512,  # after tokenization
     min_len: int = 8,  # after tokenization
+    canonical: bool = True,
+    isomeric: bool = False,
     filter_unk: bool = True,
     num_proc: int = 4,
     num_shards: int = 10,
+    cache_dir: Path = None,
 ):
+    files = list(data_path.glob("*.txt")) if data_path.is_dir() else [data_path]
     tok: PreTrainedTokenizerFast = PreTrainedTokenizerFast.from_pretrained(
         pretrained_tokenizer
     )
     unk_id = tok.unk_token_id
     print("### unk_id:", unk_id)
-
-    dataset = datasets.load_from_disk(dataset_path)
-    dataset.set_format("np")
-    print("before filter:", dataset)
-
-    def filter_fn(smiles_list):
-        tok_output = tok.batch_encode_plus(
-            smiles_list.tolist(),
-            return_token_type_ids=False,
-            return_attention_mask=False,
-            return_length=True,
-        )
-        lens = np.array(tok_output["length"])
-        len_filter = (lens <= max_len) & (lens >= min_len)
-        if filter_unk:
-            unk_filter = np.array([not (unk_id in x) for x in tok_output["input_ids"]])
-            return len_filter & unk_filter
-        return len_filter
-
-    dataset = dataset.filter(
-        filter_fn,
-        input_columns=["smiles"],
-        batched=True,
-        batch_size=10000,
-        num_proc=num_proc,
+    composed_filter = partial(
+        filter_func,
+        tokenizer=tok,
+        unk_id=unk_id,
+        max_len=max_len,
+        min_len=min_len,
+        canonical=canonical,
+        isomeric=isomeric,
+        filter_unk=filter_unk,
     )
+    composed_iterator = partial(file_iterator, smiles_filter=composed_filter)
+
+    dataset = datasets.Dataset.from_generator(
+        composed_iterator,
+        gen_kwargs={
+            "files": files,
+        },
+        num_proc=num_proc,
+        cache_dir=cache_dir,
+    )
+
     dataset.set_format("np")
-    print("after filter:", dataset)
     dataset.save_to_disk(save_path, num_shards=num_shards, num_proc=num_proc)
-
-
-def normalize_smiles(smiles: str, canonical: bool = True, isomeric: bool = False):
-    try:
-        res = Chem.MolToSmiles(
-            Chem.MolFromSmiles(smiles),
-            canonical=canonical,
-            isomericSmiles=isomeric,
-        )
-    except:
-        return ""
-    return res
 
 
 @app.command()
-def canonicalize(
-    dataset_path: Path,
-    save_path: Path,
-    canonical: bool = True,
-    isomeric: bool = False,
-    batch_size: int = 10000,
-    num_proc: int = 4,
-    num_shards: int = 10,
-):
-
-    dataset = datasets.load_from_disk(dataset_path)
-    dataset.set_format("np")
-
-    def canonicalize_fn(smiles_list):
-        return {
-            "smiles": np.array(
-                [
-                    normalize_smiles(x, canonical=canonical, isomeric=isomeric)
-                    for x in smiles_list
-                ]
-            )
-        }
-
-    dataset = dataset.map(
-        canonicalize_fn,
-        input_columns=["smiles"],
-        batched=True,
-        batch_size=batch_size,
-        num_proc=num_proc,
-    ).filter(
-        lambda x: x != "",
-        input_columns=["smiles"],
-        batched=True,
-        batch_size=batch_size,
-        num_proc=num_proc,
-    )
-
-    dataset.set_format("np")
-    dataset.save_to_disk(save_path, num_shards=num_shards, num_proc=num_proc)
+def placeholder():
+    pass
 
 
 if __name__ == "__main__":
